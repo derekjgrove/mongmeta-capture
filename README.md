7b4fbd93806a50f213ff680cdaa3f2992c2d8494
Derek Grove <derekjgrove@gmail.com>
2024-05-15 12:14:37 -0400

    distinct profiler output | converge performance advisor and profiler points

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 # mongmeta-capture
-mongmeta-capture uses MongoDB API to request DDL and DML definitions of your cluster. Data captured is a snapshot of what you see in a MongoDB client such as MongoDBCompass
+mongmeta-capture includes multiple utilities to capture cluster level statistics.
 
-## data capture
+## DB data capture
 ```shell
-cd <PATH_TO_THIS_PACKAGE>
+cd <PATH_TO_THIS_PACKAGE>/scripts/main.js
 # set allowSampleDoc=false if you don't want me to output sample document from each collection
 mongosh "mongodb+srv://<user>:<pass>@<cluster_uri>" --eval 'var allowSampleDoc = true' --file main.js
 ```
 
-## viewer
+### viewer
 There is an `appScript.js` that can input the block of text generated from the shell into a tabular viewer in Google Doc
+
+## Profiler & Performance Advisor capture
+### Setup
+
+```shell
+cd <PATH_TO_THIS_PACKAGE>
+npm install
+```
+This script uses the Atlas Admin API to capture Profiler and Performance Advisor output. The following steps must be completed:
+1. Create an Atlas Admin API key [[Instructions here](https://www.mongodb.com/docs/atlas/configure-api-access/)]
+2. Replace `GROUP_ID` and `API_KEY` in the file `scripts\atlasAdmin\profilernPerformanceAdvisor.js`
+3. If you run the script and find you have more than one cluster in the project you are running, you will need to add the hosts that you don't want in the `FILTER_HOSTS` array of the same script file which will be shown in the output. 
+
+
+### Running
+```shell
+cd <PATH_TO_THIS_PACKAGE>/scripts/atlasAdmin
+node profilernPerformanceAdvisor.js
+```
--- scripts/atlasAdmin/profilernPerformanceAdvisor.js
+++ scripts/atlasAdmin/profilernPerformanceAdvisor.js
@@ -62,6 +62,11 @@ async function main () {
 
 
     // Get the slow queries
+    var queryHashMap = {}
+    var performanceAdvisorObj = {
+        shapes: [],
+        suggestedIndexes: []
+    }
     for (var process of processes.filter( _process => !FILTER_HOSTS.includes(_process))) {
         var processSlowQueryRes = await httpClient.request(`https://cloud.mongodb.com/api/atlas/v2/groups/${GROUP_ID}/processes/${process}/performanceAdvisor/slowQueryLogs?duration=${FIVE_DAYS}`, 
             {
@@ -76,22 +81,95 @@ async function main () {
         )
 
         var processSuggestedIndexesRes = await httpClient.request(`https://cloud.mongodb.com/api/atlas/v2/groups/${GROUP_ID}/processes/${process}/performanceAdvisor/suggestedIndexes?duration=${FIVE_DAYS}`, 
-        {
-            method: 'GET',
-            rejectUnauthorized: false,
-            digestAuth: `${API_KEY}`,
-            dataType: 'json',
-            headers: {
-                "Accept": "application/vnd.atlas.2023-11-15+json",
-            }
-        },
-    )
+            {
+                method: 'GET',
+                rejectUnauthorized: false,
+                digestAuth: `${API_KEY}`,
+                dataType: 'json',
+                headers: {
+                    "Accept": "application/vnd.atlas.2023-11-15+json",
+                }
+            },
+        )
 
-        console.log(`-------------------${process}--------------------------`)
-        console.log(processSlowQueryRes.data)
-        console.log(processSuggestedIndexesRes.data)
+        performanceAdvisorObj.shapes.push(...processSuggestedIndexesRes.data.shapes)
+        performanceAdvisorObj.suggestedIndexes.push(...processSuggestedIndexesRes.data.suggestedIndexes)
+
+        //Build unique slowQuery hashmap
+        for (var slowQuery of processSlowQueryRes.data.slowQueries) {
+            var formatedSlowQuery = JSON.parse(slowQuery.line)
+            if (formatedSlowQuery?.attr?.queryHash) {
+                if (!queryHashMap[`${slowQuery.namespace}-${formatedSlowQuery?.attr?.queryHash}`]) {
+                    queryHashMap[`${slowQuery.namespace}-${formatedSlowQuery?.attr?.queryHash}`] = {opCount : 1, ...formatedSlowQuery}
+                } else {
+                    queryHashMap[`${slowQuery.namespace}-${formatedSlowQuery?.attr?.queryHash}`].opCount += 1
+                }
+            } else {
+                if (!formatedSlowQuery.attr.command.lsid) {
+                } else {
+                if (!queryHashMap[`${slowQuery.namespace}-${formatedSlowQuery.attr.command.lsid.id["$uuid"]}`]) {
+                    queryHashMap[`${slowQuery.namespace}-${formatedSlowQuery.attr.command.lsid.id["$uuid"]}`] = {opCount : 1, ...formatedSlowQuery}
+                } else {
+                    queryHashMap[`${slowQuery.namespace}-${formatedSlowQuery.attr.command.lsid.id["$uuid"]}`].opCount += 1
+                }}
+            }
+        }
     }
 
+    // Remove Duplicate shapes and suggestedIndexes (could be duplicated across nodes)
+    performanceAdvisorObj.shapes = performanceAdvisorObj.shapes.filter((obj, index) => {
+        return index === performanceAdvisorObj.shapes.findIndex(o => obj.id === o.id);
+    });
 
+    performanceAdvisorObj.suggestedIndexes = performanceAdvisorObj.suggestedIndexes.filter((obj, index) => {
+        return index === performanceAdvisorObj.suggestedIndexes.findIndex(o => obj.id === o.id);
+    });
+
+
+    // Converge profiler and performanceAdvisor
+    var foundIndexes = []
+    var foundShapes = []
+    
+      for (var shape of performanceAdvisorObj.shapes) {
+        var shapeIndexes = performanceAdvisorObj.suggestedIndexes.filter(_suggestedIndex => _suggestedIndex.impact.includes(shape.id))
+        if (shapeIndexes.length > 0) {
+            shape.indexSuggested = shapeIndexes
+            foundIndexes.push(...shapeIndexes.map(_suggestedIndex => _suggestedIndex.id))
+        }
+    
+        for (var op of shape.operations) {
+            var shapeExplain = JSON.parse(op.raw)
+            if (shapeExplain?.attr?.queryHash) {
+                if (queryHashMap[`${shape.namespace}-${shapeExplain?.attr?.queryHash}`]) {
+                    delete op.raw
+                    queryHashMap[`${shape.namespace}-${shapeExplain?.attr?.queryHash}`].performanceAdvisor = shape
+                    foundShapes.push(shape.id)
+                }
+            } else if (shapeExplain?.attr?.command?.lsid) {
+                if (queryHashMap[`${shape.namespace}-${shapeExplain?.attr?.command?.lsid?.id["$uuid"]}`]) {
+                    delete op.raw
+                    queryHashMap[`${shape.namespace}-${shapeExplain?.attr?.command?.lsid?.id["$uuid"]}`].performanceAdvisor = shape
+                    foundShapes.push(shape.id)
+                }
+            }
+        }
+    
+    }
+
+    // Remove all merged performance advisor entries (left over did not have profiler record)
+    // Profiler goes back 5 days, Performance Advisor goes back 21 days
+    performanceAdvisorObj.suggestedIndexes = performanceAdvisorObj.suggestedIndexes.filter(_suggestedIndex => !foundIndexes.includes(_suggestedIndex.id))
+    performanceAdvisorObj.shapes = performanceAdvisorObj.shapes.filter(_shape => !foundShapes.includes(_shape.id))
+    
+    
+    
+    queryHashMap['performanceAdvisorLeftOver'] = performanceAdvisorObj
+    console.log(JSON.stringify(queryHashMap))
 }
 main()
+
+
+
+
+
+// TODO redacter - https://medium.com/@stheodorejohn/recursive-value-transformation-in-javascript-unleash-the-power-of-data-manipulation-20b7f8ad3578
